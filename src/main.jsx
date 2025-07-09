befecd0d2050a4efc638a887203daf328950e6d4
PakornPK <sweetie_jimmie@hotmail.com>
2025-07-09 21:03:15 +0700

    saparate router out of main

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { RouterProvider } from "react-router";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import router from "./route";
 import './index.css'
-import Home from "./pages/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "*", // fallback
-    element: <Home/>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
