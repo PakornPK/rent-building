8a6f8fc81d4d8c0a3731caae658c36e56b1d775a
PakornPK <sweetie_jimmie@hotmail.com>
2025-07-17 00:01:12 +0700

    add component upload file

---
+++ src/components/UploadFile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UploadFile() {
+    return (
+        <>
+            <div className='flex '>
+                <label className='bg-blue-400 text-white rounded-l-lg p-1 px-2 text-nowrap'>Upload File</label>
+                <input
+                    className='bg-neutral-100 w-full hover:bg-neutral-300 text-black rounded-r-lg p-1'  
+                    type='file' 
+                />
+            </div>
+        </>
+    )
+}
+
+export default UploadFile
