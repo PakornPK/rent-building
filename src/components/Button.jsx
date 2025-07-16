ff0a3a59933a5272e304a0f150dc543ad9fb734b
PakornPK <sweetie_jimmie@hotmail.com>
2025-07-17 00:01:47 +0700

    update style button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ className, onClick,children }) => {
     return (
         <>
             <button
-                className={className + ' bg-blue-400 p-4 text-white rounded-full'}
+                className={className + ' bg-blue-400 hover:bg-blue-600 py-2 px-4 text-white rounded-full'}
                 onClick={onClick}
             >
                 {children}
