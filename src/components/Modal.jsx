de61ae2c2bdc56b1e213404c145acae9b7952be7
PakornPK <sweetie_jimmie@hotmail.com>
2025-07-17 00:02:17 +0700

    update style modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-const Modal = ({ children }) => {
+const Modal = ({className, children }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-300/80">
-      <div className="bg-white p-6 rounded-lg shadow-lg relative max-w-md w-full">
+      <div className={className + " bg-white p-6 rounded-lg shadow-lg relative w-full"}>
         {children}
       </div>
     </div>
