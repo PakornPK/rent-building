f02dd2dd9ca1f1ba4de37d168f014e0045e04953
PakornPK <sweetie_jimmie@hotmail.com>
2025-08-21 22:22:45 +0700

    fix logout

--- src/backend/server/http.go
+++ src/backend/server/http.go
@@ -51,8 +51,10 @@ func initializedRouter(app *fiber.App, handler *handler, cfg configs.Config) {
 
 	app.Use(recover.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
-		AllowHeaders: "Origin, Content-Type, Accept",
+		AllowOrigins:     "http://localhost:5173",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true,
 	}))
 
 	authMiddleware := middlewares.AuthMiddleware(&cfg.Auth)
