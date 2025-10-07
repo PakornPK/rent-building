ae8a51df40fef0413102fac6965e93f2bbee05c2
PakornPK <sweetie_jimmie@hotmail.com>
2025-10-07 23:59:58 +0700

    add room api

---
+++ src/backend/internal/handlers/room_handler.go
@@ -0,0 +1,140 @@
+package handlers
+
+import (
+	"github.com/PakornPK/rent-building/internal/services"
+	"github.com/PakornPK/rent-building/logger"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+type RoomHandler interface {
+	CreateRoom(c *fiber.Ctx) error
+	ListRoom(c *fiber.Ctx) error
+	GetRoom(c *fiber.Ctx) error
+	UpdateRoom(c *fiber.Ctx) error
+	DeleteRoom(c *fiber.Ctx) error
+	AppendRoomRental(c *fiber.Ctx) error
+	RemoveRoomRental(c *fiber.Ctx) error
+}
+
+type roomHandler struct {
+	service services.RoomRentalService
+	logger  logger.Logger
+}
+
+func NewRoomHandler(service services.RoomRentalService, logger logger.Logger) RoomHandler {
+	return &roomHandler{
+		service: service,
+		logger:  logger,
+	}
+}
+
+func (h *roomHandler) CreateRoom(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	req := make([]services.RoomInput, 0)
+	if err := c.BodyParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	err := h.service.CreateRoom(req...)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	logger.Info("room created successfully")
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *roomHandler) ListRoom(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	input, err := paginationInputBuilder(c.Queries())
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	users, err := h.service.List(input)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to list users"})
+	}
+	logger.Info("room listed successfully")
+	return c.Status(fiber.StatusOK).JSON(users)
+}
+
+func (h *roomHandler) GetRoom(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid user ID"})
+	}
+	room, err := h.service.GetByID(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update user"})
+	}
+	logger.Info("room updated successfully")
+	return c.JSON(room)
+}
+
+func (h *roomHandler) UpdateRoom(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid user ID"})
+	}
+	var input services.RoomInput
+	if err := c.BodyParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input: " + err.Error()})
+	}
+	if err := h.service.Update(id, input); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update user"})
+	}
+	logger.Info("room updated successfully")
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
+func (h *roomHandler) DeleteRoom(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid user ID"})
+	}
+
+	if err := h.service.DeleteByID(id); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update user"})
+	}
+	logger.Info("room deleted successfully")
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *roomHandler) AppendRoomRental(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	req := make([]services.RoomRentalInput, 0)
+	if err := c.BodyParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	err := h.service.AppendRental(req...)
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	logger.Info("room rental added successfully")
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *roomHandler) RemoveRoomRental(c *fiber.Ctx) error {
+	requestID := c.Get("Request-Id", uuid.New().String())
+	logger := h.logger.WithRequestID(requestID).WithUserID(c.Locals("userID").(int))
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid user ID"})
+	}
+
+	if err := h.service.RemoveRental(id); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update user"})
+	}
+	logger.Info("room rental deletd successfully")
+	return c.SendStatus(fiber.StatusOK)
+}
